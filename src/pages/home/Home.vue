d71912f7456c96ca978ee44d4ab9053bb948cd03
One-AnDong <one-andong@foxmail.com>
2018-12-21 13:20:13 +0800

    add icons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,18 +2,21 @@
   <div>
     <home-header></home-header>
     <home-carrousel></home-carrousel>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeCarrousel from './components/Carrousel'
+import HomeIcons from './components/Icons'
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeCarrousel
+    HomeCarrousel,
+    HomeIcons
   }
 }
 </script>
