46a564ae2235cf11d75eba2157ac3d5c25f57e3a
One-AnDong <one-andong@foxmail.com>
2019-01-15 14:40:52 +0800

    add gif

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:8081',
+        target: 'http://localhost:8080',
         pathRewrite: {
           '^/api': '/static/mock'
         }
