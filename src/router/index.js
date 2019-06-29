cfbcd30ef5ce6364c733fee8b4ea435fd824d33d
One-AnDong <one-andong@foxmail.com>
2019-06-29 22:15:58 +0800

    修正文件、

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import Citiy from '@/pages/citiy/Citiy'
-
 Vue.use(Router)
 
 export default new Router({
@@ -17,7 +16,7 @@ export default new Router({
   }, {
     path: '/detail/:id',
     name: 'Detail',
-    component: () => import('@/pages/detail/Detail')
+    component: () => import('@/pages/detail/Detail') // 异步加载
   }],
   scrollBehavior (to, from, savedPosition) {
     if (savedPosition) {
